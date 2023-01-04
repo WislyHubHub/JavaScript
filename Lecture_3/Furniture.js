ba0b798185e0884e90bad00275588587910c517c
WislyHubHub <rachapruek12345@gmail.com>
2023-01-04 12:03:20 +0700

    update

---
+++ Lecture_3/Furniture.js
@@ -0,0 +1,17 @@
+let furniture = ['Table' ,'Chairs' ,'Chuch'] ;
+
+
+Array.furniture.map.call ('Table' ,'Chairs' ,'Chuch' ,eachLetter => eachLetter) ;
+
+
+//let str = [] ;
+
+//let iterator = furniture[Symbol.iterator]() ;
+
+for (let index = 0 ; index < furniture.length ; index++) { 
+    str[0] = furniture ;
+    str-- ;
+    console.log (str) ;
+
+}
+
